90743841c42af3ad3aa3501770926088c17d7a14
Aamna Ashraf <159810000+aamnaashraf@users.noreply.github.com>
2026-01-26 20:13:18 +0500

    Remove duplicate output configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   experimental: {
   },
   images: {
@@ -9,7 +10,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
-  output: 'export',
-}
+  
+};
 
 module.exports = nextConfig
